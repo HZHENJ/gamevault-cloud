fdfb3adecf5cba8047d031c4def7ba526efc3251
HUANG ZHENJIA <E1352450@u.nus.edu>
2025-10-25 23:11:50 +0800

    chore: fix pom.xml

--- gamevault-auth/src/main/java/com/sg/nusiss/auth/config/SecurityConfig.java
+++ gamevault-auth/src/main/java/com/sg/nusiss/auth/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
        http.csrf(
 
                csrf -> csrf.disable())
-               .cors(cors -> cors.disable())
+               .cors(Customizer.withDefaults())
                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                .authorizeHttpRequests(a -> a
                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
@@ -75,18 +75,18 @@ public class SecurityConfig {
 //        };
 //    }
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration cfg = new CorsConfiguration();
-//        // 与前端同源；如果之后有域名，就换成域名
-//        cfg.setAllowedOrigins(List.of("http://52.77.169.8:30131"));
-//        cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
-//        cfg.setAllowedHeaders(List.of("*"));
-//        cfg.setAllowCredentials(true);
-//        cfg.setMaxAge(3600L);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", cfg);
-//        return source;
-//    }
+   @Bean
+   CorsConfigurationSource corsConfigurationSource() {
+       CorsConfiguration cfg = new CorsConfiguration();
+       // 与前端同源；如果之后有域名，就换成域名
+       cfg.setAllowedOrigins(List.of("http://52.77.169.8:30131"));
+       cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
+       cfg.setAllowedHeaders(List.of("*"));
+       cfg.setAllowCredentials(true);
+       cfg.setMaxAge(3600L);
+
+       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+       source.registerCorsConfiguration("/**", cfg);
+       return source;
+   }
 }
