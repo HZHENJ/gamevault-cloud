76c423c8f874b736ef8a63ad969b3df19729f017
HUANG ZHENJIA <E1352450@u.nus.edu>
2025-10-25 23:27:11 +0800

    chore: fix pom.xml

---
+++ gamevault-gateway/src/main/java/com/sg/nusiss/gateway/config/CorsGlobalFilter.java
@@ -0,0 +1,40 @@
+package com.sg.nusiss.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class CorsGlobalFilter implements WebFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        ServerHttpResponse response = exchange.getResponse();
+        HttpHeaders headers = response.getHeaders();
+
+        headers.add("Access-Control-Allow-Origin", "http://52.77.169.8:30130");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+        headers.add("Access-Control-Allow-Headers", "*");
+        headers.add("Access-Control-Allow-Credentials", "true");
+
+        // 预检请求直接返回
+        if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
+            response.setStatusCode(HttpStatus.OK);
+            return response.setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -100; // 高优先级，让 CORS 在 Security 之前处理
+    }
+}
