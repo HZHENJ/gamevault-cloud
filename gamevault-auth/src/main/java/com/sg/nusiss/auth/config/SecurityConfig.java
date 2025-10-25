11986aa67c1212db9de9358e693be651f227bcc6
HUANG ZHENJIA <E1352450@u.nus.edu>
2025-10-25 17:58:03 +0800

    chore: fix pom.xml

--- gamevault-auth/src/main/java/com/sg/nusiss/auth/config/SecurityConfig.java
+++ gamevault-auth/src/main/java/com/sg/nusiss/auth/config/SecurityConfig.java
@@ -53,18 +53,18 @@ public class SecurityConfig {
         return converter;
     }
 
-    // @Bean
-    // public WebMvcConfigurer corsConfigurer() {
-    //     return new WebMvcConfigurer() {
-    //         @Override
-    //         public void addCorsMappings(CorsRegistry registry) {
-    //             registry.addMapping("/**")
-    //                     .allowedOriginPatterns("*")
-    //                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-    //                     .allowedHeaders("*")
-    //                     .allowCredentials(true)
-    //                     .maxAge(3600);
-    //         }
-    //     };
-    // }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
 }
