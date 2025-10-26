68c3830afe0c5f874ae16b70964df326c7a13456
Automan1218 <2984335643@qq.com>
2025-10-26 17:17:29 +0800

    add debug and enable

--- gamevault-developer/src/main/java/com/sg/nusiss/developer/DeveloperApplication.java
+++ gamevault-developer/src/main/java/com/sg/nusiss/developer/DeveloperApplication.java
@@ -3,6 +3,7 @@ package com.sg.nusiss.developer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 })
 @MapperScan("com.sg.nusiss.developer.mapper")
 @EnableScheduling
+@EnableDiscoveryClient
 public class DeveloperApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DeveloperApplication.class, args);
