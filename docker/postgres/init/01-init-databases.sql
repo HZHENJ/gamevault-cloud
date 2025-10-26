444d6d0453852d722b4e3c32ab8b69be9c352b92
Automan1218 <2984335643@qq.com>
2025-10-26 11:58:11 +0800

    try to run with no k8s: fix database

--- docker/postgres/init/01-init-databases.sql
+++ docker/postgres/init/01-init-databases.sql
@@ -8,3 +8,8 @@ CREATE DATABASE gamevault_shopping;
 CREATE DATABASE gamevault_forum;
 
 CREATE DATABASE gamevault_developer;
+
+GRANT ALL PRIVILEGES ON DATABASE gamevault_social TO gamevault_user;
+GRANT ALL PRIVILEGES ON DATABASE gamevault_shopping TO gamevault_user;
+GRANT ALL PRIVILEGES ON DATABASE gamevault_forum TO gamevault_user;
+GRANT ALL PRIVILEGES ON DATABASE gamevault_developer TO gamevault_user;
--- gamevault-auth/src/main/java/com/sg/nusiss/auth/config/SecurityConfig.java
+++ gamevault-auth/src/main/java/com/sg/nusiss/auth/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -71,7 +72,10 @@ public class SecurityConfig {
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
         cfg.setMaxAge(3600L);
-
+        cfg.setExposedHeaders(Arrays.asList(
+                "Authorization",
+                "Content-Type"
+        ));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", cfg);
         return source;
